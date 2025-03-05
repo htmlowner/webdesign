f9fb14c5630a0bc37eba301ad6381f20efb8e240
htmlowner <162366056+htmlowner@users.noreply.github.com>
2025-03-05 14:05:34 +0300

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,6 @@
 let users = {};
 
-fetch('../config/users.json')
+fetch('..webdsign/config/users.json')
     .then(response => {
         if (!response.ok) {
             throw new Error('Сеть не в порядке: ' + response.statusText);
